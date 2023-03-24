7d14463ab9d99ddc4bc935fa9116da718e802433
stuntmannen <117079747+danjoh2019@users.noreply.github.com>
2023-03-24 11:38:53 +0100

    new tests added

--- test/translators.js
+++ test/translators.js
@@ -23,4 +23,21 @@ describe("Translators", function () {
             assert.strictEqual(toNormal(toRobber(input)), input)
         })
     })
+    describe("#toAllsprak", function () {
+        it("should work for empty string", function () {
+            assert.strictEqual(toNormal(""), "")
+        })
+        it("should work for regular input", function () {
+            const input = "Hejsan hoppsan!"
+            assert.strictEqual(toNormal(input), input)
+        })
+        it("should work for robber language", function () {
+            const input = "Hejsan hoppsan!"
+            assert.strictEqual(toNormal(toRobber(input)), input)
+        })
+        it("should work for simple input", function() {
+            const input = "Hejsan hoppsan!"
+            assert.strictEqual(toAllsprak(input), "Hallejsan halloppsan!")
+        })
+    })
 })
